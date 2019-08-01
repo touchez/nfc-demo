ead974dc0ad45761787cc8ba11989e9b287572af
Article23399 <240634015@qq.com>
2019-08-01 21:59:12 +0800

    add sth new

--- miniprogram/pages/daoyouji2/daoyouji2.js
+++ miniprogram/pages/daoyouji2/daoyouji2.js
@@ -64,7 +64,8 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //需要传入guideMachineName
+    //需要传入guideMachineName如
+    //guideMachineName=安特生考察路线图
     this.getCloudData(options.guideMachineName);
   },
 })
